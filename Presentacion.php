484000ac85c5afdb3f339cd97aa2730021f1e8ca
Northking <jcnl2812@gmail.com>
2019-09-19 15:47:21 -0400

    commit tarea1

---
+++ Presentacion.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h1>Mi nombre es <span style='color:blue'> Jean Carlos Nuñez Liriano </span> </h1>";
+
+    echo "<h2>Expreriencia en el area</h2>", '<hr>';
+
+    echo "<p> Mi experiencia con php, html y demas tecnologias realacionadas es limitada. 
+           Si he trabajado en varios proyectos en php y conozco lo mas basico del lenguaje.</p>";
+
+    echo "<h2>Mis hobbies</h2>", '<hr>';
+
+    echo "<p> Las actividades que mas me gustan hacer como hobbies son las siguientes:
+           <ul>
+              <li>Jugar videojuegos</li>
+              <li>Ver series y peliculas</li>
+              <li>Navegar en las redes sociales</li>
+              <li>Investigar sobre cualquier tema de mi interes</li>
+              <li>Leer libros y comics</li>
+              <li>Escuchar musica</li>
+           </ul>
+          </p>";
+    ?>
+
+</body>
+</html>
